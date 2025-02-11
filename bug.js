6c5abc60fdc7280bd3e23ff8d679bca1aec78e75
BugHunter-X <askzenai@gmail.com>
2025-02-11 16:46:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using AsyncStorage in React Native and attempting to store a value that is not a string. AsyncStorage only supports storing strings.  Attempting to store other data types such as objects or numbers will result in this error, often manifesting as a silent failure or unexpected behavior. 
